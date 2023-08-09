cc5b768c1e7ee12286324bc1b71960b8157e4536
hp <hp@DESKTOP-F0LG1DD>
2023-08-09 14:04:05 +0530

    Adresss changes done

---
+++ Oyester/src/test/java/createStudentProfileTest/AddressOfStudentTest.java
@@ -0,0 +1,11 @@
+package createStudentProfileTest;
+
+public class AddressOfStudentTest {
+ 
+	public static void main(String[] args) {
+		
+		System.out.println("git");
+		
+		////,mmxcmzxcm.,zm
+	}
+}
